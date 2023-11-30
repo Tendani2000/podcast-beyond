7af64c2a6a251847212ce36029c82094f0374ccc
Tendani2000 <t.ramanyimi@lab.clicklearning.org>
2023-11-30 13:22:46 +0200

    added Scroll to top button

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,8 @@ export default function Header () {
     <header className='header'>
         <h2 className='header--title'>Podcasts Beyond</h2>
         <div className="search--it">
-            <input type="text" placeholder="Search..."></input>
+            <input type="text" 
+            placeholder="Search..." ></input>
         </div>
    
     </header>
--- src/Show.js
+++ src/Show.js
@@ -84,6 +84,7 @@ function Show () {
                         </li>
                 ))}
             </ul>
+            <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
        </div> 
         
     )
